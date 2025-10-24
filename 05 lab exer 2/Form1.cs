e487bce78d6799a1d2b10b9692251ecc3a49a584
jassyv4 <kjasvillanovar@gmail.com>
2025-10-24 20:58:44 +0800

    Add project files.

---
+++ 05 lab exer 2/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05_lab_exer_2
+{
+    public partial class FrmOpenTextFile : Form
+    {
+        public FrmOpenTextFile()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e) {
+          
+        }
+        }
+        }
+
